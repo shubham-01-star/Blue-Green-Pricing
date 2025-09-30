4a73b5054f63826bbac6c60f2e6e90cb62d99c3f
Shubham Kumar <skshubham8348@gmail.com>
2025-09-30 14:12:17 +0530

    issue fixed for devlopment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +13,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+  }
 })
